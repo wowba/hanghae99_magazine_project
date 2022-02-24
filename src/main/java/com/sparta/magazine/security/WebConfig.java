a17168c92269ac4e8b68e459b539cdcd8f4eba2d
wowba <87873821+wowba@users.noreply.github.com>
2022-02-24 10:04:24 +0900

    CORS 문제 PUT 추가

--- src/main/java/com/sparta/magazine/security/WebConfig.java
+++ src/main/java/com/sparta/magazine/security/WebConfig.java
@@ -27,6 +27,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET","POST","DELETE");
+                .allowedMethods("GET","POST","DELETE","PUT");
     }
 }
